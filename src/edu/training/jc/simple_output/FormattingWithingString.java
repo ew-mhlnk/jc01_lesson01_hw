63f7e20ebad17b74435728d10ab9dffd63de4e15
Ew Mhlnk <100856701+ew-mhlnk@users.noreply.github.com>
2025-02-25 22:29:12 +0300

    Add files via upload

---
+++ src/edu/training/jc/simple_output/FormattingWithingString.java
@@ -0,0 +1,10 @@
+package edu.training.jc.simple_output;
+
+public class FormattingWithingString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1) Todoist - баланс между мощностью и простотой.\n2) TickTick - встроенные календари и таймеры.\n3) Microsoft To Do - для опытных пользователей Microsoft.");
+	}
+
+}
---
+++ src/edu/training/jc/simple_output/TextOutput.java
@@ -0,0 +1,13 @@
+package edu.training.jc.simple_output;
+
+public class TextOutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\tLong, long ago there lived a young \nand very beautiful girl called Cinderella.");
+		System.out.println("\tCinderella lived with her stepmother \nand stepsisters. The sisters were wicked and \nhaughty.");
+
+
+	}
+
+}
